7a272d47654cf1b50e7dcef1582b1ac7c1e18c8d
amaddream <laobaoshihundan@126.com>
2015-10-29 12:58:53 +0800

    add tree-linkage

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # ng-checkbox-tree
-一个用于展现包含checkbox的无限树形结构的angular指令,需引入jquery
+一个用于展现包含checkbox的无限树形结构的angular指令,需引入jquery。
+
 
 ##用法如下：
 
+引入模块：
 ```html
-	<ng-checkbox-tree tree-data = 'treeData' tree-selected= "treeSelected"></ng-checkbox-tree>
+	angular.module('trial', ['ngCheckboxTree']);
+```
+html中:
+```html
+	<ng-checkbox-tree tree-data = 'treeData' tree-selected= "treeSelected" tree-linkage="on"></ng-checkbox-tree>
 ```
 
 ##treeData
-treeData是传入指令的数据，数据类型为数组，数组所包含的对象需要有id,title,nodes三个属性，可定义如下：
+treeData是传入指令的数据，与外界$scope绑定。数据类型为数组，数组所包含的对象需要有id,title,nodes三个属性，其中nodes也是个treeData数组，可以为空。定义如下：
 ```html
 	$scope.treeData = [{
 	    "id": 1,
@@ -59,7 +65,10 @@ treeData是传入指令的数据，数据类型为数组，数组所包含的对
 ```
 
 ##treeSelected
-treeSelected是一个数组，数组里的元素为选中的checkbox所在的node的id值
+treeSelected是一个数组，数组里的元素为选中的checkbox所在的node的id值，与外界$scope绑定。
 
-##structure
+##tree-linkage
+tree-linkage是一个属性，取值类型为字符串。当它的值为"on",nodes父子层级间的选定状态呈联动相关，否则取消联动。
+
+##指令HTML结构
 整个tree最终被包含在一个class="ng-checkbox-tree"的div中。每个node都是一个<li></li>，可以据此自定义它的样式。
--- ng-checkbox-tree.directive.js
+++ ng-checkbox-tree.directive.js
@@ -13,9 +13,11 @@
             },
             require: '?^ngModel',
             restrict: 'E',
+            replace: 'true',
             template: '<div class="ng-checkbox-tree"></div>',
             transclude: false,
             link: function(scope, ele, attrs, controller) {
+                console.log(attrs);
                 var indexTree = angular.element(ele);
 
                 //监控treeDtata
@@ -24,7 +26,7 @@
                     createBranch(scope.treeData, indexTree);
                     //
                     indexTree.find('input').bind('change', function() {
-                        select($(this));
+                        select(angular.element(this));
                     });
                 }, true);
 
@@ -47,56 +49,61 @@
                     var currentUl = checkbox.parent().parent();
                     var currentsLi = currentUl.children('li');
                     var childrenCheckbox = checkbox.parent().find('input');
-                    if (checkbox.is(':checked')) {
-                        //若一个复选框选中，它的所有子选框自动选中；
-                        childrenCheckbox.each(function() {
-                            $(this).prop('checked', true);
-                        });
-                        //
-                        selectParents(currentsLi);
-                    } else {
-                        //若一个复选框从选中状态切换到没有选中，则为其所有子选框取消选中状态。
-                        childrenCheckbox.each(function() {
-                            $(this).prop('checked', false);
-                        });
-
-                        cancelParents(currentsLi);
+                    if (attrs.treeLinkage === "on") {
+                        linkage ();
                     }
-
                     //check数据导出
                     var checkSet = indexTree.find('input');
                     scope.treeSelected = [];
                     checkSet.each(function() {
-                        if ($(this).is(':checked')) {
-                            scope.treeSelected.push($(this).val());
+                        if (angular.element(this).is(':checked')) {
+                            scope.treeSelected.push(angular.element(this).val());
                         }
                     });
                     scope.$apply();
                     //若一个复选框选中，且同级复选框都为选中状态，父级复选框自动选中，此行为递归至条件不满足为止
-                    function selectParents(currentsLi) {
-                        var currentAllSelected = true;
-                        currentsLi.each(function() {
-                            if (!$(this).children('input').is(':checked')) {
-                                currentAllSelected = false;
+                    function linkage () {
+                        if (checkbox.is(':checked')) {
+                            //若一个复选框选中，它的所有子选框自动选中；
+                            childrenCheckbox.each(function() {
+                                angular.element(this).prop('checked', true);
+                            });
+                            //
+                            selectParents(currentsLi);
+                        } else {
+                            //若一个复选框从选中状态切换到没有选中，则为其所有子选框取消选中状态。
+                            childrenCheckbox.each(function() {
+                                angular.element(this).prop('checked', false);
+                            });
+
+                            cancelParents(currentsLi);
+                        }
+
+                        function selectParents(currentsLi) {
+                            var currentAllSelected = true;
+                            currentsLi.each(function() {
+                                if (!angular.element(this).children('input').is(':checked')) {
+                                    currentAllSelected = false;
+                                }
+                            });
+                            if (currentAllSelected) {
+                                var parentLi = currentsLi.parent().parent();
+                                var parentsLi = currentsLi.parent().parent().parent().children('li');
+                                if (parentLi.length > 0) {
+                                    parentLi.children('input').prop('checked', true);
+                                    selectParents(parentsLi);
+                                }
                             }
-                        });
-                        if (currentAllSelected) {
+                        }
+
+                        function cancelParents(currentsLi) {
+                            //若一个复选框取消选中，父级复选框递归取消选中
                             var parentLi = currentsLi.parent().parent();
                             var parentsLi = currentsLi.parent().parent().parent().children('li');
                             if (parentLi.length > 0) {
-                                parentLi.children('input').prop('checked', true);
-                                selectParents(parentsLi);
+                                parentLi.children('input').prop('checked', false);
+                                cancelParents(parentsLi);
                             }
-                        }
-                    }
-
-                    function cancelParents(currentsLi) {
-                        //若一个复选框取消选中，父级复选框递归取消选中
-                        var parentLi = currentsLi.parent().parent();
-                        var parentsLi = currentsLi.parent().parent().parent().children('li');
-                        if (parentLi.length > 0) {
-                            parentLi.children('input').prop('checked', false);
-                            cancelParents(parentsLi);
                         }
                     }
 
